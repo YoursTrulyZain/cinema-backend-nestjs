8267def98b6f2c9e8d594957e86d0f6a8114d15f
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2025-05-05 22:42:03 -0400

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'cinema.z41n.dev',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
