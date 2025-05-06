5528fdd97e0489717a70d1349d21e06fc7c5f47f
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2025-05-05 21:52:13 -0400

    Update Port for Railway

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
